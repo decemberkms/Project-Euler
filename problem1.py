18db1501d0cbd8a448cae4bbb03bb1196dee7c04
Min <decemberkms@gmail.com>
2020-03-18 20:27:46 +0100

    Solved Q2

--- problem1.py
+++ problem1.py
@@ -23,4 +23,20 @@ l = []
 for i in range(3,1000):
 	if(i%3==0 or i%5==0):
 		l.append(i)
+
+// for C++ implementation
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+	int sum = 0;
+	for(int i=3; i<1000 ; i++)
+	{
+		if(i%3==0 || i%5==0)
+			sum += i;
+	}
+	cout<<sum<<endl;
+	return 0;
+}
 '''
