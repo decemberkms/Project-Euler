ba74e5c10139d225b092b88f611542b57c644512
Min <decemberkms@gmail.com>
2020-03-31 19:59:54 +0200

    Sovled Q8

--- problem7.py
+++ problem7.py
@@ -4,28 +4,49 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 '''
 
-def primeTest(n):
-    for i in range(2,n):
-        if n % i == 0:
-            return False
-    return True
+# def primeTest(n):
+#     for i in range(2,n):
+#         if n % i == 0:
+#             return False
+#     return True
+#
+# print(primeTest(20))
+#
+# t = 2
+# n = 0
+# while n != 10001:
+#     if primeTest(t) == True:
+#         print('###prime', t)
+#         t += 1
+#         print('count', n)
+#         n += 1
+#     else:
+#         print('count', n)
+#         t += 1
+#
+#
+# print('prime', t -1 , 'count', n)
 
-print(primeTest(20))
+import time
 
-t = 2
-n = 0
-while n != 10001:
-    if primeTest(t) == True:
-        print('###prime', t)
-        t += 1
-        print('count', n)
-        n += 1
-    else:
-        print('count', n)
-        n
-        t += 1
+def erato(n):
+    prime_n = []
+    prime_bool = [True for i in range(n+1)]
+    p = 2
+    while p*p <=n:
+        if(prime_bool[p]==True):
+            for i in range(p*p,n+1,p):
+                prime_bool[i]=False
+        p += 1
+    for p in range(2,n):
+       if prime_bool[p]:
+           prime_n.append(p)
 
+    return(prime_n)
 
-print('prime', t -1 , 'count', n)
-104724
-104730
+tic = time.time()
+p = erato(1000000)
+# print(p)
+# print(len(p))
+print(p[10000])
+print("Time taken = {}ms".format(1000*(time.time()-tic)))
