9797b38d947ba2be5df2f11096f972a753ab66d7
Min <decemberkms@gmail.com>
2020-04-02 20:57:50 +0200

    Add time to Q9

--- problem9.py
+++ problem9.py
@@ -7,6 +7,10 @@ For example, 32 + 42 = 9 + 16 = 25 = 52.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 '''
+import time
+#Time at the start of exection
+start_time = time.time()
+
 def pythagorean_triplet():
  for a in range(1,999):
   for b in range(1,999):
@@ -16,3 +20,9 @@ def pythagorean_triplet():
     return a*b*c
 
 print(pythagorean_triplet())
+
+#time at the end of execution
+end_time = time.time()
+
+#Printing the total_time
+print(end_time - start_time)
