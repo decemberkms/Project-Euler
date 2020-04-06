add47b3c5883553728a49883653bfc96036cb289
Min <decemberkms@gmail.com>
2020-04-06 20:27:49 +0200

    Solved Q10

--- problem10.py
+++ problem10.py
@@ -3,17 +3,28 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 '''
-def primeTest(n):
-    for i in range(2,n):
-        if n % i == 0:
-            return False
-    return True
-print(primeTest(20))
+list_p = []
+def SieveOfEratosthenes(n):
 
-n = 20000
-t = 0
-for i in range(2,n):
-    if primeTest(i):
-        t += i
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is Not a prime, else true.
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
 
-print(t)
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+
+            # Update all multiples of p
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0]= False
+    prime[1]= False
+    # Print all prime numbers
+    for p in range(n + 1):
+        if prime[p]:
+            list_p.append(p)
+SieveOfEratosthenes(2000000)
+print(sum(list_p))
